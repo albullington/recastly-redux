85f770a1af02f05c31cb130129d0d053760d2130
Amanda <albullington@gmail.com>
2018-05-22 17:09:22 -0700

    added first reducer, two actions, and store

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,11 +2,15 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-
+import store from '../store/store.js';
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  // return (dispatch) => {
+  //   setTimeout(() => {
+  //     dispatch(searchYouTube(q));
+  //   }, 2000);
+  // };
 };
 
 export default handleVideoSearch;
